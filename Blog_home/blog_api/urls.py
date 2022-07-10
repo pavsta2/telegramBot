af2ca68835a65283ca40156d474b104039ff7647
pavsta2 <92543263+pavsta2@users.noreply.github.com>
2022-07-10 17:41:31 +0300

    Delete Blog_home directory

--- Blog_home/blog_api/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('notes/', views.NoteListCreateAPIView.as_view()),
-    path('notes/<int:pk>', views.NoteDetailAPIView.as_view()),
-]
