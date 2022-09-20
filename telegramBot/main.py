3bffc8bd0ec486d12ce21d2a4a113faf642e180c
pavelstarshinov@gmail.com <pav$sta2>
2022-09-20 23:59:38 +0300

    21.09.22

--- telegramBot/main.py
+++ telegramBot/main.py
@@ -1,21 +1,23 @@
-import telebot
 import os
 import re
 
+import telebot
+from yaweather import YaWeather, Russia
+from translate import Translator
 from dotenv import load_dotenv
 from telebot import types
 
 load_dotenv()
 
 token = os.getenv('TOKEN')  # получаем токен из переменных окружения
+YANDEX_API_KEY = os.getenv('YANDEX_API_KEY')  # получаем токен api yandex погоды из переменных окружения
 bot = telebot.TeleBot(token)  # создаем экземпляр бота
 
 
 # создаем декорированную функцию для обработки сообщений от пользователя
 @bot.message_handler(commands=['start', 'MENU'])
 def start(message):
-    hello_mess = f'Здравствуйте, {message.from_user.first_name}!\n' \
-                 f'Я помощник навигации по сайту клубов HIROBOTS. ' \
+    hello_mess = f'Здравствуйте, {message.from_user.first_name}!\n Я бот-помощник сети детских клубов HiROBOTS. ' \
                  f'Задайте Ваш вопрос или напишите мне цифру, соответствующую интересующему Вас вопросу: \n'
     with open("menu_mess.txt", encoding="utf-8") as f:
         menu_mess = f.read()
@@ -36,32 +38,35 @@ def got_text(message):
 
     # вопросы про расписание занятий
     if re.search(r'расписани', text) is not None or text == '2':
-        markup_shed = types.InlineKeyboardMarkup(row_width=1)
-        btn_avia = types.InlineKeyboardButton('пр.Авиаконструкторов д.2',
-                                              url='https://www.hirobots.spb.ru/raspisanie-kulba-aviakonstruktorov')
-        btn_gash = types.InlineKeyboardButton('ул.Ярослава Гашека д.17',
-                                              url='https://www.hirobots.spb.ru/raspisanie-kluba-gasheka')
-        btn_vo = types.InlineKeyboardButton('Малый пр.В.О. д.88',
-                                            url='https://www.hirobots.spb.ru/расписание-клуба-василеостровский')
-
-        markup_shed.add(btn_avia, btn_gash, btn_vo)
-        bot.send_message(message.chat.id, 'Выберите адрес клуба:', reply_markup=markup_shed)
+        # markup_shed = types.InlineKeyboardMarkup(row_width=1)
+        # btn_avia = types.InlineKeyboardButton('пр.Авиаконструкторов д.2',
+        #                                       url='https://www.hirobots.spb.ru/raspisanie-kulba-aviakonstruktorov')
+        # btn_gash = types.InlineKeyboardButton('ул.Ярослава Гашека д.17',
+        #                                       url='https://www.hirobots.spb.ru/raspisanie-kluba-gasheka')
+        # btn_vo = types.InlineKeyboardButton('Малый пр.В.О. д.88',
+        #                                     url='https://www.hirobots.spb.ru/расписание-клуба-василеостровский')
+        #
+        # markup_shed.add(btn_avia, btn_gash, btn_vo)
+        # bot.send_message(message.chat.id, 'Выберите адрес клуба:', reply_markup=markup_shed)
+        photo1 = open('photo/kupchino 22-23 - 2.jpg', 'rb')
+        photo2 = open('photo/Avia 22-23.jpg', 'rb')
+        photo3 = open('photo/VO 22-23.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo=photo1)
+        bot.send_photo(message.chat.id, photo=photo2)
+        bot.send_photo(message.chat.id, photo=photo3)
 
     # вопросы про то, как найти клуб
-    elif re.search(r'вас найти', text) is not None or \
+    elif re.search(r'найти', text) is not None or \
             re.search(r'где.+клуб', text) is not None or \
             re.search(r'найти.+клуб', text) is not None or \
+            re.search(r'какой.+адрес', text) is not None or \
             re.search(r'находит', text) is not None:
-        markup_shed = types.InlineKeyboardMarkup(row_width=1)
-        btn_avia = types.InlineKeyboardButton('пр.Авиаконструкторов д.2',
-                                              url='https://www.hirobots.spb.ru/raspisanie-kulba-aviakonstruktorov')
-        btn_gash = types.InlineKeyboardButton('ул.Ярослава Гашека д.17',
-                                              url='https://www.hirobots.spb.ru/raspisanie-kluba-gasheka')
-        btn_vo = types.InlineKeyboardButton('Малый пр.В.О. д.88',
-                                            url='https://www.hirobots.spb.ru/расписание-клуба-василеостровский')
-
-        markup_shed.add(btn_avia, btn_gash, btn_vo)
-        bot.send_message(message.chat.id, 'Какой клуб Вы ищете?:', reply_markup=markup_shed)
+        photo1 = open('photo/Gasheka_how_to_find.png', 'rb')
+        photo2 = open('photo/Avia_how_to_find.png', 'rb')
+        photo3 = open('photo/VO_how_to_find.png', 'rb')
+        bot.send_photo(message.chat.id, photo=photo1)
+        bot.send_photo(message.chat.id, photo=photo2)
+        bot.send_photo(message.chat.id, photo=photo3)
 
     # вопросы про то, как записаться в клуб, как подать заявку
     elif re.search('запис', text) is not None \
@@ -69,24 +74,32 @@ def got_text(message):
             or text == '3':
         markup_reg = types.InlineKeyboardMarkup(row_width=1)
         btn_reg = types.InlineKeyboardButton('Запись на пробное',
-                                             url='https://www.hirobots.spb.ru/services')
+                                             url='https://hirobots.spb.ru/order')
         markup_reg.add(btn_reg)
         bot.send_message(message.chat.id, 'Перейдите в раздел сайта он-лайн записи, '
                                           'заполните поля и отправьте заявку:', reply_markup=markup_reg)
 
-    # вопросы про то, как оплатить, какая стоимость, какая цена
-    elif re.search(r'опла[т,ч]', text) is not None or \
-            re.search(r'заплат', text) is not None or \
-            re.search(r'стои[т,м]', text) is not None or \
+    # вопросы про то, какая стоимость, какая цена
+    elif re.search(r'стои[т,м]', text) is not None or \
             re.search(r'цен[а,ы,е,у]', text) is not None or \
-            text == '1' or \
-            text == '4':
+            text == '1':
         markup_price = types.InlineKeyboardMarkup(row_width=1)
         btn_price = types.InlineKeyboardButton('Стоимость и формы оплаты',
-                                               url='https://www.hirobots.spb.ru/stoimost')
+                                               url='https://hirobots.spb.ru/price')
         markup_price.add(btn_price)
-        bot.send_message(message.chat.id, 'О стоимости абонементов и способах его оплаты '
+        bot.send_message(message.chat.id, 'О стоимости абонементов'
                                           'можно узнать в разделе:', reply_markup=markup_price)
+    # вопросы про то, как оплатить
+    elif re.search(r'опла[т,ч]', text) is not None or \
+            re.search(r'заплат', text) is not None or \
+            text == '4':
+        markup_price = types.InlineKeyboardMarkup(row_width=1)
+        btn_price = types.InlineKeyboardButton('Способы оплаты',
+                                               url='https://hirobots.spb.ru/pay')
+        markup_price.add(btn_price)
+        bot.send_message(message.chat.id, 'О способах оплаты '
+                                          'можно узнать в разделе:', reply_markup=markup_price)
+
 
     # вопросы про бесплатное пробное занятие
     elif re.search(r'пробн', text) is not None or \
@@ -96,7 +109,7 @@ def got_text(message):
             text == '3':
         markup_test = types.InlineKeyboardMarkup(row_width=1)
         btn4 = types.InlineKeyboardButton('Бесплатное пробное занятие',
-                                          url='https://www.hirobots.spb.ru/probnoye')
+                                          url='https://hirobots.spb.ru/free-lesson')
         markup_test.add(btn4)
         bot.send_message(message.chat.id, 'Всем новичкам мы предлагаем бесплатное пробное занятие. '
                                           'Подробнее о пробном занятии, что взять с собой в разделе:',
@@ -154,15 +167,7 @@ def got_text(message):
             mess = f.read()
         bot.send_message(message.chat.id, mess)
 
-    elif text == '10':
-        markup_news = types.InlineKeyboardMarkup(row_width=1)
-        btn_news = types.InlineKeyboardButton('Новости',
-                                              url='https://www.hirobots.spb.ru/blog-1')
-        markup_news.add(btn_news)
-        bot.send_message(message.chat.id, 'Обо всех новостях наших клубов '
-                                          'можно узнать в разделе:', reply_markup=markup_news)
-
-    # вопросы про то, как с связаться, контакты администратора, номер телефона и пр.
+    # вопросы про контакты
     elif re.search(r'администра', text) is not None \
             or re.search(r'звонить', text) is not None \
             or re.search(r'связаться', text) is not None \
@@ -171,19 +176,35 @@ def got_text(message):
             or text == '9':
         markup_cont = types.InlineKeyboardMarkup(row_width=1)
         btn_price = types.InlineKeyboardButton('Контакты',
-                                               url='https://www.hirobots.spb.ru/contact')
+                                               url='https://hirobots.spb.ru/#97173d92-8711-4379-b9aa-653ecd552e9e')
         markup_cont.add(btn_price)
         bot.send_message(message.chat.id, 'Все наши контактные данные Вы можете '
                                           'найти в разделе:', reply_markup=markup_cont)
+    #запрос погоды
+    elif re.search(r'погода', text) is not None or text == '10':
+        api_key = os.environ.get("YANDEX_API_KEY")
+        y = YaWeather(api_key=api_key)
+        res = y.forecast(Russia.SaintPetersburg)
+        tr = Translator(from_lang='English', to_lang='Russian') #объект класса Translator для перевода погоды с анг
+
+        forecast = ''
+        for f in res.forecasts:
+            day = f.parts.day_short
+            forecast += f'{f.date} | {day.temp} °C, {tr.translate(day.condition)}\n'
+
+        bot.send_message(message.chat.id, f'Сейчас: {res.fact.temp} °C, ощущается как {res.fact.feels_like} °C \n'
+                                          f'Погодные условия: {tr.translate(res.fact.condition)}')
+        bot.send_message(message.chat.id, f'Погода на неделю:\n {forecast}')
+
 
     # во всех остальных случаях бот предлагает перейти в раздел контактов
     else:
         markup_oops = types.InlineKeyboardMarkup(row_width=1)
         btn_cont = types.InlineKeyboardButton('Контакты',
-                                              url='https://www.hirobots.spb.ru/contact')
+                                              url='https://hirobots.spb.ru/#97173d92-8711-4379-b9aa-653ecd552e9e')
         markup_oops.add(btn_cont)
         bot.send_message(message.chat.id, 'К сожалению, я не знаю ответ на этот вопрос! '
-                                          'Попробуйте спросить еще раз или позвоните нам. '
+                                          'Попробуйте перефразировать вопрос или позвоните нам. '
                                           'Все контакты указаны в разделе:', reply_markup=markup_oops)
 
 
